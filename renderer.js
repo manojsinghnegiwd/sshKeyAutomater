7607c258b3f31adf13c96d87922f74eaec882604
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-09-30 11:02:28 +0530

    generated ssh

--- renderer.js
+++ renderer.js
@@ -1,3 +1,38 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+var exec = require('child_process').exec
+var fs = require('fs');
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        path: '',
+        passphrase: '',
+        pub_key: '',
+    },
+    methods: {
+    	generateKey: function () {
+
+    		const cmd = 'ssh-keygen -N "' + this.passphrase + '" -f' + this.path;
+
+    		const self = this;
+
+    		var child = exec(cmd,
+		    (error, stdout, stderr) => {
+		        if(stdout!==''){
+		            fs.readFile(this.path + '.pub', 'utf8', (err, data) => {
+		            	console.log(data)
+		            })
+		        }
+		        if(stderr!==''){
+		            console.log('---------stderr: ---------\n' + stderr);
+		        }
+		        if (error !== null) {
+		            console.log('---------exec error: ---------\n[' + error+']');
+		        }
+		    });
+    	}
+    }
+})
